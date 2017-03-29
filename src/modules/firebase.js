d25c3621cdd16792530294585a33f683f14f9206
Thomas Dashney <tdashney@gmail.com>
2017-03-28 21:31:13 -0400

    fix weird firebase issues

--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/database'
 import { firebase as firebaseConfig } from 'config'
 
 firebase.initializeApp(firebaseConfig)

84df26c7a8dda80e04883900687d9552950d1fec
Thomas Dashney <tdashney@gmail.com>
2017-05-25 22:18:28 -0400

    update/shorten bio

--- src/sections/home/Home.js
+++ src/sections/home/Home.js
@@ -16,34 +16,18 @@ class Home extends Component {
         <img src={gardenPhotoUrl} alt='Lost Cousins' />
         <p>
           Family isn’t always bound by blood. In the case of Lost Cousins
-          it is bound through the kindred connection found in their unique
-          blend of psychedelic soul rock. Coming together as students in
-          Kingston, ON, Dylan Cantlon Hay, Cam Duffin, Lloyd McArton and
-          Thomas Dashney had humble beginnings at house concerts and basement
-          performances, where they became known for their high-energy live
-          shows and memorable songwriting.
+          it is bound through the kindred connection found in their distinct
+          blend of psych rock. Coming together as students in Kingston, ON,
+          Dylan Cantlon Hay, Cam Duffin, Lloyd McArton and Thomas Dashney had
+          humble beginnings at house concerts and basement performances, where
+          they became known for their high-energy live shows and memorable songwriting.
         </p>
         <p>
-          The group released their debut EP Not Now What We Were in April of
-          2015, uprooting them from their Kingston student house to their new
-          home of Toronto, where they found themselves land in the Top 10 of
-          CBC Music’s Searchlight competition. The collection of songs
-          demonstrates the band’s journey of coming into its own, combining
-          influences from the past with soulful lyrics that express growth
-          and transformation.
-        </p>
-        <p>
-          For the past two years, Lost Cousins have toured extensively through
-          Canada and the United States, including performances at WayHome Music
-          & Arts Festival and CityFolk Festival, as well as opening for acts
-          such as The Strumbellas, Hollerado, and The Wooden Sky. This May,
-          the band is touring eastern Canada and the U.S. for a string of dates
-          in support of Saskatchewan indie rock act Close Talker. After spending
-          the winter writing and refining new material in the spare bedroom
-          studio in their east Toronto home, the band is set to release a single
-          for their song “Quarters” this coming June. Following the release, the
-          summer will see Lost Cousins making appearances at festivals such as
-          NxNE Port Lands and Hillside Festival.
+          Making use of gang vocals, delay-drenched guitars, and dreamy synthesizers,
+          the four-piece continues to experiment with sonic boundaries and sprawling
+          shifts in dynamics. Incorporating celestial textures and creative rhythms,
+          the Toronto-based quartet combines influences from the past with soulful
+          lyrics to create groove- oriented songs that feel both honest and alive.
         </p>
       </div>
     )

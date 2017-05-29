233270bfbc3e9523936b7dc5508ccd3c88677130
Thomas Dashney <tdashney@gmail.com>
2017-05-29 14:44:06 -0400

    update bio with lloyd's comments

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -19,7 +19,7 @@ class Home extends Component {
         <p>
           Family isn’t always bound by blood. In the case of Lost Cousins
           it is bound through the kindred connection found in their distinct
-          blend of psych rock. Coming together as students in Kingston, ON,
+          blend of psychedelic rock. Coming together as students in Kingston, ON,
           Dylan Cantlon Hay, Cam Duffin, Lloyd McArton and Thomas Dashney had
           humble beginnings at house concerts and basement performances, where
           they became known for their high-energy live shows and memorable songwriting.
@@ -29,7 +29,7 @@ class Home extends Component {
           the four-piece continues to experiment with sonic boundaries and sprawling
           shifts in dynamics. Incorporating celestial textures and creative rhythms,
           the Toronto-based quartet combines influences from the past with soulful
-          lyrics to create groove- oriented songs that feel both honest and alive.
+          lyrics to create groove-oriented songs that feel both honest and alive.
         </p>
       </div>
     )

893ae8cf50ffe2230644a19b2e63e4dc7a72f62c
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-01-18 14:41:25 -0500

    remove EP

--- src/app/music/releases.js
+++ src/app/music/releases.js
@@ -1,4 +1,3 @@
-import notNowWhatWeWere from './album-covers/not-now-what-we-were.jpg'
 import quarters from './album-covers/quarters.jpg'
 
 export default [
@@ -37,44 +36,6 @@ export default [
       {
         type: 'deezer',
         url: 'http://www.deezer.com/track/369095291?utm_source=deezer&utm_content=track-369095291&utm_term=1576206446_1497059822&utm_medium=web'
-      }
-    ]
-  },
-  {
-    title: 'Not Now What We Were - EP',
-    date: 'April 2015',
-    imageSrc: notNowWhatWeWere,
-    credits: [
-      'Written and Performed by Lost Cousins',
-      'Produced by Darryl Neudorf',
-      'Mastered by Peter J. Moore',
-      'Artwork by Mady Newey'
-    ],
-    spotifyURI: 'spotify:track:2qSxvTaR7lw2BYGcmnYAGz',
-    links: [
-      {
-        type: 'applemusic',
-        url: 'https://itun.es/ca/nO298'
-      },
-      {
-        type: 'spotify',
-        url: 'https://open.spotify.com/album/1XoYRnnZGUf3LVkcPTr3V3'
-      },
-      {
-        type: 'googleplay',
-        url: 'https://play.google.com/store/music/album/Lost_Cousins_Not_Now_What_We_Were?id=B7qp6eze3vdumkdstyhllecvjra'
-      },
-      {
-        type: 'bandcamp',
-        url: 'https://lostcousins.bandcamp.com/releases'
-      },
-      {
-        type: 'soundcloud',
-        url: 'https://soundcloud.com/lostcousinsband/sets/not-now-what-we-were-2015'
-      },
-      {
-        type: 'deezer',
-        url: 'http://www.deezer.com/album/10899782?utm_source=deezer&utm_content=album-10899782&utm_term=1576206446_1497059842&utm_medium=web'
       }
     ]
   }

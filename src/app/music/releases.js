06bf0d4fcfa6e4227a0dad875b24ec9496da48a6
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-02-28 12:44:58 -0500

    add feel an emotion single + art

--- src/app/music/releases.js
+++ src/app/music/releases.js
@@ -1,42 +1,64 @@
 import quarters from './album-covers/quarters.jpg'
+import feelAnEmotion from './album-covers/feel-an-emotion.jpg'
 
-export default [
-  {
-    title: 'Quarters - Single',
-    date: 'June 2017',
-    imageSrc: quarters,
-    credits: [
-      'Written and Performed by Lost Cousins',
-      'Produced by Nixon Boyd',
-      'Mastered by Noah Mintz',
-      'Artwork by Jati Putra'
-    ],
-    spotifyURI: 'spotify:album:4C3ICoHRTXVd3MLhdssjsl',
-    links: [
-      {
-        type: 'applemusic',
-        url: 'https://itun.es/ca/z42fkb'
-      },
-      {
-        type: 'spotify',
-        url: 'https://open.spotify.com/album/4C3ICoHRTXVd3MLhdssjsl'
-      },
-      {
-        type: 'googleplay',
-        url: 'https://goo.gl/TExMYT'
-      },
-      {
-        type: 'bandcamp',
-        url: 'https://lostcousins.bandcamp.com/track/quarters-single'
-      },
-      {
-        type: 'soundcloud',
-        url: 'https://soundcloud.com/lostcousinsband/quarters'
-      },
-      {
-        type: 'deezer',
-        url: 'http://www.deezer.com/track/369095291?utm_source=deezer&utm_content=track-369095291&utm_term=1576206446_1497059822&utm_medium=web'
-      }
-    ]
-  }
-]
+export default [{
+  title: 'Quarters - Single',
+  date: 'June 2017',
+  imageSrc: quarters,
+  credits: [
+    'Written and Performed by Lost Cousins',
+    'Produced by Nixon Boyd',
+    'Mastered by Noah Mintz',
+    'Artwork by Jati Putra'
+  ],
+  spotifyURI: 'spotify:album:4C3ICoHRTXVd3MLhdssjsl',
+  links: [{
+    type: 'applemusic',
+    url: 'https://itun.es/ca/z42fkb'
+  }, {
+    type: 'spotify',
+    url: 'https://open.spotify.com/album/4C3ICoHRTXVd3MLhdssjsl'
+  }, {
+    type: 'googleplay',
+    url: 'https://goo.gl/TExMYT'
+  }, {
+    type: 'bandcamp',
+    url: 'https://lostcousins.bandcamp.com/track/quarters-single'
+  }, {
+    type: 'soundcloud',
+    url: 'https://soundcloud.com/lostcousinsband/quarters'
+  }, {
+    type: 'deezer',
+    url: 'http://www.deezer.com/track/369095291?utm_source=deezer&utm_content=track-369095291&utm_term=1576206446_1497059822&utm_medium=web'
+  }]
+}, {
+  title: 'Feel an Emotion - Single',
+  date: 'April 2015',
+  imageSrc: feelAnEmotion,
+  credits: [
+    'Written and Performed by Lost Cousins',
+    'Produced by Darryl Neudorf',
+    'Mastered by Peter J. Moore',
+    'Artwork by Jati Putra'
+  ],
+  spotifyURI: 'spotify:album:2SPttlD02wiVP2Z9lmJNGM',
+  links: [{
+    type: 'applemusic',
+    url: 'https://itunes.apple.com/ca/album/feel-an-emotion/1352099012'
+  }, {
+    type: 'spotify',
+    url: 'https://open.spotify.com/album/2SPttlD02wiVP2Z9lmJNGM?si=tmzl6ESOR3yHiE7-rYEX2w'
+  }, {
+    type: 'googleplay',
+    url: 'https://play.google.com/store/music/album/Lost_Cousins_Feel_an_Emotion?id=B5xd2w4jfohfvkk5z2yk76c6smi'
+  }, {
+    type: 'bandcamp',
+    url: 'https://lostcousins.bandcamp.com/track/feel-an-emotion'
+  }, {
+    type: 'soundcloud',
+    url: 'https://soundcloud.com/lostcousinsband/feel-an-emotion'
+  }, {
+    type: 'deezer',
+    url: 'http://www.deezer.com/album/57757882?utm_source=deezer&utm_content=album-57757882&utm_term=1576206446_1519839722&utm_medium=web'
+  }]
+}]
